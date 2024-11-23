0e7d89b4990272052a03438b066ea0cae9d63125
rokib97 <rokibulhasan.ph@gmail.com>
2024-11-23 20:19:19 +0600

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import pluginJs from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ['node_modules', 'dist/**/*'],
+    rules: {
+      'no-unused-vars': 'error',
+    },
+  },
+  {
+    files: ['dist/**/*.js'],
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
+      'no-unused-expressions': 'off',
+    },
+  },
+];
